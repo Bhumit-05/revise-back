357a4244da0a5e721840262b5c64fc0f971dc361
Bhumit Janghel <bhumit05janghel@gmail.com>
2024-12-05 15:15:41 +0530

    added path

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { TodoModel } = require("./db.js");
 const cors = require("cors");
+const path = require('path');
 
 
 const app = express();
