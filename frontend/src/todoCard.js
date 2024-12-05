8a3ba18d1ad04a1ce11b29c4382458471f30621d
Bhumit Janghel <bhumit05janghel@gmail.com>
2024-12-05 17:49:44 +0530

    updated request links 2

--- frontend/src/todoCard.js
+++ frontend/src/todoCard.js
@@ -8,14 +8,14 @@ const TodoCard = (todo) => {
   const dispatch = useDispatch();
 
   const fetchTodo = async () => {
-    const res = await fetch("http://localhost:4000/todo");
+    const res = await fetch("https://revise-back.onrender.com");
     const data = await res.json();
 
     dispatch(addTodo(data));
   };
 
   async function handleRemove(){
-    await fetch("http://localhost:4000/todo",{
+    await fetch("https://revise-back.onrender.com",{
       method : "DELETE",
       headers: {
         "Content-Type": "application/json", 
